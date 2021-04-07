753eb23a7209d83be9f9837f18a23ecaf6e14a97
jyyblue <jyyblue1987@outlook.com>
2021-04-07 09:09:23 +0800

    [create] create react frontend.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require('./db');
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
 const app = express()
-const port = 3000
+app.use(express.static(path.join(__dirname, 'build')));
+
+
 
 app.get('/', (req, res) => {
-  
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

8a0e8e3b53a782252a39d4994e88fca4460802e8
jyyblue <jyyblue1987@outlook.com>
2021-04-07 09:17:11 +0800

    [add] add react routing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React111
-        </a>
-      </header>
-    </div>
+    <Router>
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/login">Login</Link>
+					</li>
+					<li>
+						<Link to="/register">Users</Link>
+					</li>
+				</ul>
+			</nav>
+
+			{/* A <Switch> looks through its children <Route>s and
+				renders the first one that matches the current URL. */}
+			<Switch>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+    </Router>
   );
+}
+
+function Home() {
+	return <h2>Home</h2>;
+}
+
+function Login() {
+	return <h2>Login</h2>;
+}
+
+function Register() {
+	return <h2>Register</h2>;
 }
 
 export default App;
